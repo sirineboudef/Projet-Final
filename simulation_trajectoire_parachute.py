edb7350d746d3e3c5b367cb306a247a408502682
syrine <syrine.boudef.1@ens.etsmtl.ca>
2025-06-20 15:58:32 -0400

    Importations des bibliotheques necessaires pour le module de simulation et l'optimisation de la trajectoire

---
+++ simulation_trajectoire_parachute.py
@@ -0,0 +1,9 @@
+#Importations necessaires
+import numpy as np
+from importer_vent import import_vent# <== fichier où se trouve ta fonction précédente
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')  # Pour ouvrir une fenêtre interactive
+from matplotlib.animation import FuncAnimation, PillowWriter
+
+#
