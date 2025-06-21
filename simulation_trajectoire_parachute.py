502e4ff95be0c9bb800c12b13abfad6ba45a00aa
syrine <syrine.boudef.1@ens.etsmtl.ca>
2025-06-20 23:34:34 -0400

    Implementation de l'optimisation

--- simulation_trajectoire_parachute.py
+++ simulation_trajectoire_parachute.py
@@ -73,3 +73,66 @@ def simuler_trajectoire(lat=47.3388, lon=-81.9141, N=31):
     const += [u_bar[:, [k]].T @ u[:, [k]] - v[k] >= -eps_h for k in range(0, N)]
     const += [cvx.norm(u[:, [k]]) - v[k] <= eps_h for k in range(0, N)]
 
+
+    # === 1. Définir la cible ===
+    target = np.array([[lat], [lon]])
+    final_position = cvx.norm(x[:, [-1]] - target)
+
+    # final_position = cvx.norm(x[:, [-1]])
+    final_angle = 2 - u[1, [-1]] / np.linalg.norm(v[-1])
+    control_cost = cvx.sum_squares(cvx.norm(cvx.diff(u, axis=1), axis=0) / v[0:N - 1]) / dt
+    cost = alpha_1 * final_position + alpha_2 * final_angle + control_cost
+    MAX_ITER = 50
+
+    it_final_position = np.empty((MAX_ITER))
+    it_final_angle = np.empty((MAX_ITER))
+    it_control_cost = np.empty((MAX_ITER))
+    it_cost = np.empty((MAX_ITER))
+    X = np.empty((2, N, MAX_ITER))
+    U = np.empty((2, N, MAX_ITER))
+    D = np.empty((N - 1, MAX_ITER))
+
+    problem = cvx.Problem(cvx.Minimize(cost), const + [eps_h == 0.1])
+    first_stage_converged = False
+
+    print('Iteration number\t Final position\t Final angle\t Control cost\t Total cost')
+    for i in range(MAX_ITER):
+
+        s = problem.solve(solver=cvx.ECOS, verbose=True, warm_start=True)
+        u_bar.value = np.divide(u.value, np.linalg.norm(u.value, axis=0))
+
+        x_star = x.value
+        u_star = u.value
+
+        X[:, :, i] = x_star
+        U[:, :, i] = u_star
+
+        it_final_position[i] = final_position.value
+        it_final_angle[i] = final_angle.value
+        it_control_cost[i] = control_cost.value
+        it_cost[i] = cost.value
+
+        print(str(i) + '\t' + '\t' + '\t' + "%10.3E" % it_final_position[i] + '\t' + "%10.3E" % it_final_angle[
+            i] + '\t' + "%10.3E" % it_control_cost[i] + '\t' + "%10.3E" % it_cost[i])
+        if (np.abs(it_cost[i] - it_cost[i - 1]) < eps_convergence) and first_stage_converged:
+            print("STAGE 2 CONVERGED AFTER " + str(i) + " ITERATIONS")
+            n_iter = i
+
+            # === Affichage de l’erreur finale ===
+            target = np.array([lat, lon])
+            xf = X[0, -1, n_iter]  # dernière position en x
+            yf = X[1, -1, n_iter]  # dernière position en y
+
+            tx, ty = target[0], target[1]
+            erreur = np.sqrt((xf - tx) ** 2 + (yf - ty) ** 2)
+
+            print(f"📍 Point d'atterrissage : ({xf:.2f}, {yf:.2f})")
+            print(f"🎯 Erreur par rapport à la cible : {erreur:.2f} m")
+
+            break
+        if (i > 1) and (np.abs(it_cost[i] - it_cost[i - 1]) < eps_convergence) and not first_stage_converged:
+            print("STAGE 1 CONVERGED AFTER " + str(i) + " ITERATIONS")
+            cost = cost + alpha_3 * eps_h
+            problem = cvx.Problem(cvx.Minimize(cost), const)
+            first_stage_converged = True
+            n_iter_first = i
