594e9ffbc0847107f62e4900327c4070c6257ef3
Linda Ghazouani <lindaghazouani520@gmail.com>
2025-06-22 19:30:09 -0400

    definition de la methode import vent (recuperation des donnees de l'api et creation du profil de vent) en POO

--- importer_vent.py
+++ importer_vent.py
@@ -22,3 +22,45 @@ class ImportVent:
         angle_rad = np.radians(direction_deg)
         return speed_ms * np.sin(angle_rad), speed_ms * np.cos(angle_rad)
 
+    def import_vent(self):
+        tf = self.t0 + np.sqrt(self.ch) / self.rz0 / np.sqrt(self.rho0) * (
+            ((1 - self.z0 * self.cz) ** self.cf) / self.cf / self.cz - ((1 - 0 * self.cz) ** self.cf) / self.cf / self.cz)
+        time = np.linspace(0, tf, self.N)
+
+        z_t = 1 / self.cz * (
+            1 - ((((1 - self.z0 * self.cz) ** self.cf) / self.cf / self.cz -
+                 (time - self.t0) * self.rz0 * np.sqrt(self.rho0) / np.sqrt(self.ch)) *
+                 self.cf * self.cz) ** (1 / self.cf))
+
+        altitudes_api = [10, 80, 120, 180]
+        url = (
+            f"https://api.open-meteo.com/v1/forecast?"
+            f"latitude={self.lat}&longitude={self.lon}"
+            f"&hourly=wind_speed_10m,wind_direction_10m,"
+            f"wind_speed_80m,wind_direction_80m,"
+            f"wind_speed_120m,wind_direction_120m,"
+            f"wind_speed_180m,wind_direction_180m"
+            f"&timezone=auto"
+        )
+
+        response = requests.get(url)
+        data = response.json()
+
+        vx_profiles = []
+        vy_profiles = []
+        for a in altitudes_api:
+            speed = data['hourly'][f'wind_speed_{a}m'][self.hour_index]
+            direction = data['hourly'][f'wind_direction_{a}m'][self.hour_index]
+            vx, vy = self.convert_to_vx_vy(speed, direction)
+            vx_profiles.append(vx)
+            vy_profiles.append(vy)
+
+        vx_interp = np.interp(z_t, altitudes_api, vx_profiles)
+        vy_interp = np.interp(z_t, altitudes_api, vy_profiles)
+
+        W = np.array([vx_interp, vy_interp])
+        return W, z_t, time, data
+
+
+def import_vent(lat, lon, hour_index=0, N=31, z0=1200):
+    return ImportVent(lat, lon, hour_index, N, z0).import_vent()
