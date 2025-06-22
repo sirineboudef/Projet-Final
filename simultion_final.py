2b4251d9ed115ea17b5606b1911a3dc50968e404
syrine <syrine.boudef.1@ens.etsmtl.ca>
2025-06-22 19:58:55 -0400

    1- Ajout de la methode principale: simuler_trajectoire.

--- simultion_final.py
+++ simultion_final.py
@@ -34,3 +34,77 @@ class SimulationTrajectoire:
     def calcul_profil_vitesse(self, z):
         return self.vz0 * np.sqrt(self.rho0 / self.calcul_densite(z))
 
+    def simuler_trajectoire(self):
+        W, z_t, time, _ = import_vent(self.lat, self.lon, self.N)
+        self.time = time
+        self.z_t = z_t
+
+        tf = self.time[-1]
+        dt = tf / (self.N - 1)
+        z = self.calcul_altitude(self.time)
+        v = self.calcul_profil_vitesse(z)
+
+        A = np.eye(2, 2)
+        B_p = np.eye(2, 2) * dt * 0.5
+        B_m = np.eye(2, 2) * dt * 0.5
+        phid_max = 0.14
+        u_0 = np.array([[v[0] * np.cos(self.psi_0)], [v[0] * np.sin(self.psi_0)]])
+
+        eps_convergence = 0.01
+        alpha_1, alpha_2, alpha_3 = 100, 10, 1
+
+        x = cvx.Variable((2, self.N))
+        u = cvx.Variable((2, self.N))
+        eps_h = cvx.Variable(nonneg=True)
+        u_bar = cvx.Parameter((2, self.N))
+        u_init = np.array([v * np.cos(self.psi_0), v * np.sin(self.psi_0)])
+        norms = np.linalg.norm(u_init, axis=0)
+        norms[norms == 0] = 1e-6
+        u_bar.value = np.divide(u_init, norms)
+
+        const = [x[:, [0]] == self.x_0, u[:, [0]] == u_0]
+        const += [x[:, [k + 1]] == A @ x[:, [k]] + (B_m @ u[:, [k]] + B_p @ u[:, [k + 1]]) + [W[:, k]]
+                  for k in range(self.N - 1)]
+        const += [(cvx.norm2(cvx.diff(u, axis=1), axis=0) / dt / v[k])[k] <= phid_max for k in range(self.N - 1)]
+        const += [u_bar[:, [k]].T @ u[:, [k]] - v[k] >= -eps_h for k in range(self.N)]
+        const += [cvx.norm(u[:, [k]]) - v[k] <= eps_h for k in range(self.N)]
+
+        target = np.array([[self.lat], [self.lon]])
+        final_position = cvx.norm(x[:, [-1]] - target)
+        final_angle = 2 - u[1, [-1]] / np.linalg.norm(v[-1])
+        control_cost = cvx.sum_squares(cvx.norm(cvx.diff(u, axis=1), axis=0) / v[:-1]) / dt
+        cost = alpha_1 * final_position + alpha_2 * final_angle + control_cost
+
+        MAX_ITER = 50
+        it_cost = np.empty(MAX_ITER)
+        X = np.empty((2, self.N, MAX_ITER))
+
+        problem = cvx.Problem(cvx.Minimize(cost), const + [eps_h == 0.1])
+        first_stage_converged = False
+
+        for i in range(MAX_ITER):
+            s = problem.solve(solver=cvx.ECOS, verbose=True, warm_start=True)
+            if u.value is None:
+                raise ValueError(f"ECOS n'a pas trouvé de solution à l'itération {i}")
+
+            u_bar.value = np.divide(u.value, np.linalg.norm(u.value, axis=0))
+
+            X[:, :, i] = x.value
+            it_cost[i] = cost.value
+
+            if (i > 0 and abs(it_cost[i] - it_cost[i - 1]) < eps_convergence):
+                if first_stage_converged:
+                    n_iter = i
+                    break
+                else:
+                    cost += alpha_3 * eps_h
+                    problem = cvx.Problem(cvx.Minimize(cost), const)
+                    first_stage_converged = True
+
+        self.x_star = X[:, :, n_iter]
+        self.z_t = z_t
+        self.time = time
+        self.target = np.array([self.lat, self.lon])
+        self.n_iter = n_iter
+        return self.x_star, self.calcul_erreur(), (self.x_star[0, -1], self.x_star[1, -1]), self.z_t, self.time
+
