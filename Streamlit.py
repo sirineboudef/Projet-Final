4ca5ddef7461398ae514f9e2eb308115d7bfec27
wilson <wilson-david.parra-oliveros.1@ens.etsmtl.ca>
2025-06-23 15:58:38 -0400

    Ajout de la fonction background pour définir une image d'arriére plan animée (gif)

--- Streamlit.py
+++ Streamlit.py
@@ -24,3 +24,26 @@ import pandas as pd
 
 # plotly.express : permet de créer facilement des graphiques interactifs (ex : courbes, cartes, etc.)
 import plotly.express as px
+
+# Fonction pour définir une image d'arrière-plan animée (GIF) dans l'application Streamlit
+def set_background_image():
+    # CSS intégré pour personnaliser l'apparence de l'application (stApp est la classe principale de Streamlit)
+    page_bg_img = '''
+    <style>
+    .stApp {
+        /* URL de l'image d'arrière-plan (ici un GIF depuis Giphy) */
+        background-image: url("https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzIxcWV6OHVhb25yNDB6NXBoY29uaGU3a2tnMWFvdDRxbXQ5Z2lsZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7qDKdHAqamtq0uBi/giphy.gif");
+
+        /* L'image couvre toute la surface de l'écran */
+        background-size: cover;
+
+        /* Centre l'image sur la page */
+        background-position: center;
+
+        /* Empêche l'image de se répéter */
+        background-repeat: no-repeat;
+    }
+    </style>
+    '''
+    # Injection du style CSS dans la page via Markdown, avec autorisation du HTML
+    st.markdown(page_bg_img, unsafe_allow_html=True)
